2d0ccd1ea6e88c8cb6ad3e8d2710931a8ff81bf9
Cody Garrett <cody.l.garrett22@gmail.com>
2019-10-14 15:17:57 -0600

    project complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Route} from "react-router-dom";
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
+const App  = () => (
   <div>
     <Navigation />
+    <Route path="/" exact component={Home}></Route>
+    <Route path="/About" component={About}></Route>
+    <Route path="/Contact" component={Contact}></Route>
   </div>
 );
 
